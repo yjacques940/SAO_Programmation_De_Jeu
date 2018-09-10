84d3b2f1a5f4ce511d45bce21ff8365cdaeee023
Yannick <yannick.jacques940@gmail.com>
2018-09-10 17:20:34 -0400

    Ennemies-Création de capsule+ennemy pour tester le chasing et le changement d'animations

---
+++ Assets/EnnemyAI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnnemyAI : MonoBehaviour
+{
+    public Transform Player;
+    public Transform CurrentEnnemy;
+    public bool Chasing = true;
+    int MoveSpeed = 3;
+    public UnityEngine.AI.NavMeshAgent agent;
+    public AnimationClip Run;
+    public AnimationClip Attack;
+    public float MinimumDistance = 3;
+    // Use this for initialization
+    void Start()
+    {
+        agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = Vector3.Distance(CurrentEnnemy.position, Player.position);
+        if (Chasing)
+        {
+            GetComponent<Animation>().CrossFade(Run.name);
+        }
+        if(distance <= MinimumDistance)
+        {
+            GetComponent<Animation>().CrossFade(Attack.name);
+        }
+        agent.destination = Player.position;
+    
+    }
+}
+
---
+++ Assets/MoveCapsule.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCapsule : MonoBehaviour {
+    private Rigidbody rigidBody;
+    // Use this for initialization
+    [SerializeField] public float ForceToRight = 25;
+    [SerializeField] public float ForceToLeft = 25;
+    [SerializeField] public float ForceToUp = 25;
+    [SerializeField] public float ForceToBack = -25;
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 200.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
