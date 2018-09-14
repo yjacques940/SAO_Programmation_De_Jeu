1a59c4e66dfaef707c5c8830cdf0656dd5acc9b8
Yannick <yannick.jacques940@gmail.com>
2018-09-14 16:24:32 -0400

    Modification script pour attaquer le player

--- Assets/EnnemyAI.cs
+++ Assets/EnnemyAI.cs
@@ -6,40 +6,50 @@ using UnityEngine.UI;
 public class EnnemyAI : MonoBehaviour
 {
     public Transform Player;
-    public Transform CurrentEnnemy;
     public bool Chasing = true;
     int MoveSpeed = 3;
     public UnityEngine.AI.NavMeshAgent agent;
     public AnimationClip Run;
     public AnimationClip Attack;
-    public float MinimumDistance = 3;
-    public Animator animator;
+    public AnimationClip Idle;
+    public float MinimumDistance = 6;
     // Use this for initialization
     void Start()
     {
         agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
-        animator = gameObject.GetComponent<Animator>();
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var distance = Vector3.Distance(CurrentEnnemy.position, Player.position);
+        var distance = Vector3.Distance(agent.transform.position, Player.transform.position);
+      
         if (Chasing)
         {
             GetComponent<Animation>().CrossFade(Run.name);
         }
-        if(distance <= MinimumDistance)
+        if (distance > MinimumDistance)
         {
-            animator.SetLookAtPosition(Player.position);
+            if(agent.transform.position.x > 0)
+            {
+                agent.destination = Player.position - new Vector3(MinimumDistance/4, 0,0);
+            }
+            else
+            {
+                agent.destination = Player.position + new Vector3(MinimumDistance/4, 0,0);
+            }
+        }
+        if (distance <= MinimumDistance+1)
+        {
             GetComponent<Animation>().CrossFade(Attack.name);
-            agent.enabled = false;
+            Chasing = false;
         }
-        else
-        {
-            agent.enabled = true;
-        }
-        agent.destination = Player.position;
+        //if(!Chasing && !Attack)
+        //{
+        //    GetComponent<Animation>().CrossFade(Idle.name);
+        //}
+       
     }
 }
 
