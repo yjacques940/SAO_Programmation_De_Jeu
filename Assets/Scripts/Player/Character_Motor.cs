6ca70574e11682f51ff55a961a1da7a592cfcc82
Jessy Rodrigue <rodrigue_jes@hotmail.com>
2018-09-17 16:19:44 -0400

    Player - Refait le personnage et les contrôles

--- Assets/Scripts/Player/Character_Motor.cs
+++ Assets/Scripts/Player/Character_Motor.cs
@@ -3,49 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Character_Motor : MonoBehaviour {
+    
+    public float movingSpeed = 6.0F;
+    public float rotationSpeed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 rotateDirection = Vector3.zero;
 
-    Animation animations;
-
-    public float walkSpeed;
-    public float runSpeed;
-    public float turnSpeed;
-
-    public string inputFront;
-    public string inputBack;
-    public string inputLeft;
-    public string inputRight;
-    public string inputCrouch;
-    public string inputRun;
-
-    public Vector3 jumpSpeed;
-    CapsuleCollider playerCollider;
-
-    void Start()
-    {
-        animations = gameObject.GetComponent<Animation>();
-        playerCollider = gameObject.GetComponent<CapsuleCollider>();
-    }
+    void Start() {}
 
     void Update()
     {
-        if (Input.GetKey(inputFront))
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
         {
-            transform.Translate(0, 0, walkSpeed * Time.deltaTime);
-            animations.Play("walk");
-        }
-        else if (Input.GetKey(inputBack))
-        {
-            transform.Translate(0, 0, -(walkSpeed / 2) * Time.deltaTime);
-            animations.Play("walk");
-        }
+            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
+            if (Input.GetAxis("Fire3") > 0) moveDirection[2] *= 2F;
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= movingSpeed;
 
-        if (Input.GetKey(inputLeft))
-        {
-            transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+            rotateDirection = new Vector3(0, Input.GetAxis("Horizontal"), 0);
+            rotateDirection *= movingSpeed;
+
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
         }
-        if (Input.GetKey(inputRight))
-        {
-            transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
-        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+        transform.Rotate(rotateDirection);
+
+        //Fire1 is Left CTRL
+        //Fire3 is left Shift
     }
 }
