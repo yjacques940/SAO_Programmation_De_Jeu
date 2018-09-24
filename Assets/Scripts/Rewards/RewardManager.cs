3e8744af5abe9cae5a20b40062e6442b54e7b1b2
Vincent <vlarochelle970@gmail.com>
2018-09-24 17:01:55 -0400

    PLAYER- Classe RewardManager pour distribuer des recompenses + playerTestLoops afin de tester

---
+++ Assets/Scripts/Rewards/RewardManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//------Rework when other classes are ready
+
+public class RewardManager : MonoBehaviour {
+   
+    [SerializeField] playerTestLoops player;
+    [SerializeField] GameObject loot;
+
+
+    void Start () {
+        loot = new GameObject();
+        loot.name = "epee";      
+    }
+	
+	void Update () {
+        rewardPlayer(15,1, loot);
+	}
+
+    void rewardPlayer(int arg_experience, int arg_skill_points, GameObject arg_loot_item)
+    {    
+        player.ReceiveRewards(arg_experience, arg_skill_points, loot);     
+    }
+
+    /*GameObject getPlayer()
+    {
+        return GameObject.FindWithTag("Player");
+    }*/
+}
---
+++ Assets/playerTestLoops.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerTestLoops : MonoBehaviour {
+
+    int experience;
+    int skillPoints;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //receiveExperienceAndSkillPoints();
+        Debug.Log("I have " + experience + " and " + skillPoints + " skillpoints");
+    }
+
+    public void receiveLoot(GameObject arg_loot)
+    {
+        Debug.Log("I have received the following item: " +arg_loot.name);
+    }
+
+    public void ReceiveRewards(int arg_experience, int arg_skill_points, GameObject arg_item_loot)
+    {
+        experience += arg_experience;
+        skillPoints += arg_skill_points;
+        receiveLoot(arg_item_loot);
+    }
+}
