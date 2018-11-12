11d5710912049166e176f9ae2b572f7f50f434e5
Unknown <yannick.jacques940@gmail.com>
2018-11-12 17:14:12 -0500

    Ennemies-SpawnPoints + Régler bug du sautillage

---
+++ Assets/Scripts/SpawnPoints.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoints : MonoBehaviour {
+    
+    private void Awake()
+    {
+        LoadEnnemies();
+    }
+
+    private void LoadEnnemies()
+    {
+        GameObject monster;
+            monster = Instantiate(Resources.Load("Ennemies/MushroomMon")) as GameObject;
+            monster.transform.position = this.transform.position;
+    }
+}
