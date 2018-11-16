3ae2f82bb948c418bec72926ea7faa7399f76500
Unknown <yannick.jacques940@gmail.com>
2018-11-16 15:44:15 -0500

    Enlever bossSpawnPoint

--- Assets/BossSpawnPoint.cs
+++ Assets/BossSpawnPoint.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class BossSpawnPoint : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Awake()
+    {
+        GameObject boss;
+
+    }
 }
