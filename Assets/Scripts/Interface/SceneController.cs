e8711fc99dc7cf59150cff1843d0d9d7ca77ab1e
Jessy Rodrigue <rodrigue_jes@hotmail.com>
2018-11-16 16:34:05 -0500

    Commencé une interface et la scène du menu principal

---
+++ Assets/Scripts/Interface/SceneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+    public static SceneController sceneControl;
+
+    private void Awake() {
+        if (sceneControl == null) {
+            DontDestroyOnLoad(gameObject);
+            sceneControl = this;
+        }
+        else if (sceneControl != this) {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void ReloadCurrentScene() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void loadSelectedScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
