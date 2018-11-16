93923ba484ab7b8c28f7f5140e223524cc7f318e
Unknown <yannick.jacques940@gmail.com>
2018-11-16 14:00:43 -0500

    Enlever le using de inventory pro

--- Assets/Scripts/Player/Character_Motor.cs
+++ Assets/Scripts/Player/Character_Motor.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using Unity.Collections.LowLevel.Unsafe;
-using Devdog.InventoryPro;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
