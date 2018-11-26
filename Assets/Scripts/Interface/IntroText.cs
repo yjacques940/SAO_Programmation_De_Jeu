c61b047cd7e88ae8fff5deb89b082b3d74f46057
Jessy Rodrigue <rodrigue_jes@hotmail.com>
2018-11-26 17:22:51 -0500

    Interface - Intégrer une zone de texte pour l'introduction
    Il faut ajouter un bon texte

---
+++ Assets/Scripts/Interface/IntroText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroText : MonoBehaviour {
+
+    public GameObject IntroductionText;
+	
+	public void toggleIntroductionText()
+    {
+        IntroductionText.SetActive(!IntroductionText.activeSelf);
+    }
+
+    public void FixedUpdate()
+    {
+        if (IntroductionText.activeSelf)
+            if (IntroductionText.GetComponent<Image>() != null)
+                if (IntroductionText.GetComponent<Image>().color.a < 255) {
+                    Color color = IntroductionText.GetComponent<Image>().color;
+                    color.a += 1 * Time.deltaTime;
+                    IntroductionText.GetComponent<Image>().color = color;
+                };
+    }
+}
